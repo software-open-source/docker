3940948813c342245a3d26b0262de964ef31b2c5
Hide Dev <hide.dev.2022@gmail.com>
2025-03-11 00:37:45 +0700

    Update source code

--- admin-dashboad/vite.config.ts
+++ admin-dashboad/vite.config.ts
@@ -1,20 +1,18 @@
-import { defineConfig } from "vite";  
-import react from "@vitejs/plugin-react";  
-import svgr from "vite-plugin-svgr";  
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
-export default defineConfig({  
-  server: {  
-    host: "0.0.0.0", // Cho phép truy cập container từ mọi IP  
-    port: 3000       // Ứng dụng chạy trên cổng 3000 trong container  
-  },  
-  plugins: [  
-    react(),  
-    svgr({  
-      svgrOptions: {  
-        icon: true,  
-        exportType: "named",  
-        namedExport: "ReactComponent",  
-      },  
-    }),  
-  ],  
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        // This will transform your SVG to a React component
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
+  ],
 });
