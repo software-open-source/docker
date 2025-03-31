4e32ac462deb62149114c598f07634580b8ded52
Hide Dev <hide.dev.2022@gmail.com>
2025-03-31 21:20:39 +0700

    Config project

---
+++ admin-dashboad/src/App.tsx
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import SignIn from './pages/AuthPages/SignIn';
+import SignUp from './pages/AuthPages/SignUp';
+import NotFound from './pages/OtherPage/NotFound';
+import UserProfiles from './pages/UserProfiles';
+import Videos from './pages/UiElements/Videos';
+import Images from './pages/UiElements/Images';
+import Alerts from './pages/UiElements/Alerts';
+import Badges from './pages/UiElements/Badges';
+import Avatars from './pages/UiElements/Avatars';
+import Buttons from './pages/UiElements/Buttons';
+import LineChart from './pages/Charts/LineChart';
+import BarChart from './pages/Charts/BarChart';
+import Calendar from './pages/Calendar';
+import BasicTables from './pages/Tables/BasicTables';
+import FormElements from './pages/Forms/FormElements';
+import Blank from './pages/Blank';
+import AppLayout from './layout/AppLayout';
+import { ScrollToTop } from './components/common/ScrollToTop';
+import Home from './pages/Dashboard/Home';
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          {/* Dashboard Layout */}
+          <Route element={<AppLayout />}>
+            <Route index path="/" element={<Home />} />
+
+            {/* Others Page */}
+            <Route path="/profile" element={<UserProfiles />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
